71a9ff60b53e6f7d15b2fa13319ead35b1d69978
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-09-15 21:28:42 -0500

    added reply functionality

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -62,13 +62,13 @@ function printComment(comment) {
       <p>${comment.commentBody}</p>
       <div class="bg-dark ml-3 p-2 rounded" >
         ${
-          comment.replies && comment.replies.length
-            ? `<h5>${comment.replies.length} ${
+          comment.replies && comment.replies.length}
+            ? <h5>${comment.replies.length} ${
                 comment.replies.length === 1 ? 'Reply' : 'Replies'
               }</h5>
-        ${comment.replies.map(printReply).join('')}`
+        ${comment.replies.map(printReply).join('')}
             : '<h5 class="p-1">No replies yet!</h5>'
-        }
+        
       </div>
       <form class="reply-form mt-3" data-commentid='${comment._id}'>
         <div class="mb-3">
@@ -149,7 +149,34 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
+
+$backBtn.addEventListener('click', function() {
+  window.history.back();
+});
+
 
 $backBtn.addEventListener('click', function() {
   window.history.back();
